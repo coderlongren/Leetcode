e1523d1d9106e6a34a48791dbe26e60c7def2b28
coderlong <862983319@qq.com>
2018-01-01 22:52:32 +0800

    2018年01月 1日 周一 22:52:29

--- src/leetcode303_range_sum_Query_immutable.java
+++ src/leetcode303_range_sum_Query_immutable.java
@@ -1,7 +1,7 @@
 /**
 * @author 作者 : coderlong
 * @version 创建时间：2017年12月12日 下午8:37:05
-* 类说明: 
+* 类说明:  DP num[i]保存的就是 0-i之间的和，当然初始化num[i]是需要使用DP的，这样在计算 i -j之间的和就非常方便了， 直接num[j] - num[i]
 */
 public class leetcode303_range_sum_Query_immutable {
 
@@ -14,7 +14,7 @@ public class leetcode303_range_sum_Query_immutable {
 	public leetcode303_range_sum_Query_immutable(int[] nums) {
 	    for(int i = 1; i < nums.length; i++)
 	        nums[i] += nums[i - 1];
-	    
+	    	
 	    this.nums = nums;
 	}
 
