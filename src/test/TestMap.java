952fa501f00151ef97d353d380474ba8cc3754ab
coderlong <862983319@qq.com>
2018-01-02 22:08:41 +0800

    2018年01月 2日 周二 22:08:41

---
+++ src/test/TestMap.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Map;
+
+/**
+* @author 作者 : coderlong
+* @version 创建时间：2018年1月2日 下午10:08:12
+* 类说明: 
+*/
+public class TestMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Map.getor);
+	}
+
+}
