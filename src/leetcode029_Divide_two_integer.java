bfa502bbe3a4fa648ad2af996a979614e24b1e9e
coderlongren <coderlongren@gmail.com>
2018-01-20 22:24:00 +0800

    2018年01月20日 周六 22:23:58

---
+++ src/leetcode029_Divide_two_integer.java
@@ -0,0 +1,49 @@
+/**
+* @author 作者 : coderlong
+* @version 创建时间：2018年1月20日 下午2:17:13
+* 类说明: 
+*/
+public class leetcode029_Divide_two_integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(divide(-26, -3));
+		System.out.println(Math.abs(-2147483648));
+	}
+	//  3*8=24  28/3 ---> 24 ,4/3 --->1  ---> 1结束   
+	public static int divide(int dividend, int divisor) {
+        if (dividend == 0){
+        	return 0;
+        }
+        if(divisor == 1){
+        	return dividend;
+        }
+        if (divisor == -1){
+        	return -dividend;
+        }
+        int sign = -1;
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)){
+        	sign = 1;
+        }
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        long v1 = dividend;
+        long v2 = divisor;
+        
+        long  result = 0;
+        while (dividend >= divisor){
+        	long d = divisor;
+        	long temp = 1; // 用来保存 临时的倍数
+        	// 
+        	while (dividend > d << 1){
+        		d = d << 1; // d 每次扩大二倍，
+        		temp = temp << 1; //
+        	}
+        	result += temp;
+        	dividend -= d;
+        }
+        return (int) result*sign;
+    }
+	
+
+}
