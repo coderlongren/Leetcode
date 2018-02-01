cfb32224d90032d2b68fa6ea45ed107c46d1a89c
coderlongren <coderlongren@gmail.com>
2018-02-01 23:08:43 +0800

    2018年02月 1日 周四 23:08:42

--- src/leetcode034_Search_for_a_range.java
+++ src/leetcode034_Search_for_a_range.java
@@ -1,13 +1,54 @@
+import java.util.Arrays;
+
 /**
 * @author 作者 : coderlong
 * @version 创建时间：2018年1月31日 下午2:16:37
-* 类说明: 
+* 类说明: 类似二分查找，但等于目标值时需要向左向右对其相邻元素进行判断。
 */
 public class leetcode034_Search_for_a_range {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO A	uto-generated method stub
+		int[] nums = {5};
+		searchRange(nums, 5);
 	}
+	 public static int[] searchRange(int[] nums, int target) {
+		 int index = binaaySearch(nums, target);
+		 if (index != -1) {
+			 int left = index;
+			 int right = index;
+			 while ( left - 1 >= 0 && nums[left - 1] == target) {
+					 left--;
+			 }
+			 while (right + 1 < nums.length && nums[right + 1] == target) {
+					 right++;
+			 }
+			 return new int[] {left,right};
+		 }
+		 else {
+			 return new int[] {-1, -1};
+		 }
+	        
+	 }
+	 public static int binaaySearch(int[] nums, int target) {
+		 int left = 0;
+		 int right = nums.length - 1;
+		 int index = -1;
+		 while (left <= right) {
+			 int mid = (left + right)/2;
+			 if (nums[mid] == target) {
+				 index = mid;
+				 break;
+			 }
+			 else if (nums[mid] < target) {
+				 left = mid + 1;
+			 }
+			 else {
+				 right = mid - 1;
+			 }
+		 }
+		 return index;
+	 }
+	 
 
 }
---
+++ src/leetcode036_Valid_Sudoku.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+* @author 作者 : coderlong
+* @version 创建时间：2018年2月1日 下午10:50:17
+* 类说明: 
+*/
+public class leetcode036_Valid_Sudoku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public boolean isValidSudoku(char[][] board) {
+		// 这道题想了很久，觉得解法太奇妙了。
+		// 原理是 如何检查每一行，每一列，每一个小的九方格的 重复情况
+		for (int i = 0; i < 9; i++) {
+			Set<Character> rowSet = new HashSet<>();
+			Set<Character> colSet = new HashSet<>();
+			Set<Character> suduSet = new HashSet<>();
+			
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] != '.' && !rowSet.add(board[i][j])) {
+					return false;
+				}
+				if (board[j][i] != '.' && !colSet.add(board[j][i])) {
+					return false;
+				}
+				int sudu_row = i/3*3 + j/3;
+				int sudu_col = i%3*3 + j%3;
+				if (board[sudu_row][sudu_col] != '.' && !suduSet.add(board[sudu_row][sudu_col])) {
+					return false;
+				}
+						 
+			}
+		}
+		return true;
+	}
+
+}
